f5db721f09a1462bdb5d3231bdc4728aac12f13c
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2025-05-31 15:56:35 -0300

    :recycle: refactor: Adiciona propriedades Cpf, Salario, Login e Senha à classe UsuarioDTO.

--- AkariBeauty/Objects/Dtos/Entities/UsuarioDTO.cs
+++ AkariBeauty/Objects/Dtos/Entities/UsuarioDTO.cs
@@ -1,12 +1,16 @@
-﻿namespace AkariBeauty.Objects.Dtos.Entities
+﻿using AkariBeauty.Objects.Enums;
+
+namespace AkariBeauty.Objects.Dtos.Entities
 {
     public class UsuarioDTO
     {
         public string Id { get; set; }
         public string Nome { get; set; }
-        public string Cargo { get; set; }
-        public string Email { get; set; }
-        public string Telefone { get; set; }
+        public string Cpf { get; set; }
+        public string Salario { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public TipoUsuario TipoUsuario { get; set; }
         public int EmpresaId { get; set; }
     }
 
